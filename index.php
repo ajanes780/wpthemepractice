dafb44a030f2868e1fdac9b4076e8285304e7e44
Aaron Janes <Aaron.Janes@gmail.com>
2021-04-23 14:57:03 -0600

    inital commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+  function greet($name, $color){
+    echo "<p>hello my name is $name , $color</p>";
+  }
+greet("aaron", "green")
+?>
+
+
+<h1> <?php bloginfo("name"); ?></h1>
+<p> <?php bloginfo("description");?></p>
+
