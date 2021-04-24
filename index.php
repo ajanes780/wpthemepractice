8014f8f294c972eea496268860eb520a7153e2ae
Aaron Janes <Aaron.Janes@gmail.com>
2021-04-23 19:18:24 -0600

    basic layout

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
-<?php 
-  function greet($name, $color){
-    echo "<p>hello my name is $name , $color</p>";
-  }
-greet("aaron", "green")
+<?php get_header();
+
+  while(have_posts()) {
+    the_post(); ?>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <?php the_content(); ?>
+    <hr>
+  <?php }
+
+  get_footer();
+
 ?>
 
 
-<h1> <?php bloginfo("name"); ?></h1>
-<p> <?php bloginfo("description");?></p>
+
+
+
 
